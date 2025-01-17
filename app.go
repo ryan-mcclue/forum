5a146abd4e272e4899c28fdba4d6e69aff36c638
Ryan McClue <re.mcclue@protonmail.com>
2025-01-17 13:32:25 +1100

    db

--- app.go
+++ app.go
@@ -7,16 +7,57 @@ import (
 	"go.hasen.dev/vbolt"
 )
 
+var dbInfo vbolt.Info
+
+type User struct {
+	Id int
+	Username string
+	Email string
+	IsAdmin bool
+}
+
+// buckets (map)
+// indexes (bidirectional multimap: many-to-manys?) 
+// collections (hierarchy of keys?)
+
+// both serialisation/deserialisation
+func PackUser(self *User, buf *vpack.Buffer) {
+	vpack.Version(1, buf)
+	vpack.Int(&self.Id, buf)
+	vpack.String(&self.Username, buf)
+	vpack.String(&self.Email, buf)
+	vpack.Bool(&self.IsAdmin, buf)
+}
+
+// fixed int key
+var UsersBkt = vbolt.Bucket(&dbInfo, "users", vpack.FInt, PackUser)
+var PasswdBkt = vbolt.Bucket(&dbInfo, "passwd", vpack.FInt, vpack.ByteSlice)
+var UsernameBkt = vbolt.Bucket(&dbInfo, "username", vpack.StringZ, vpack.Int)
+
+// NOTE: no pagination
+func fetchUsers(tx *vbolt.Tx) (users []User) {
+	vbolt.IterateAll(tx, UsersBkt, func(key int, value User) bool {
+		generic.Append(&users, value)
+		return true
+	})
+	return
+}
+
 // local server from filesystem, dev from RAM
 func MakeApplication() *vbeam.Application {
 	// will shutdown existing server if running on same port
 	vbeam.RunBackServer(cfg.Backport)
 
 	db := vbolt.Open(cfg.DBPath)
+	vbolt.WithWriteTx(db, func(tx *vbolt.Tx) {
+		vbolt.TxRawBucket(tx, "proc")
+		vbolt.EnsureBuckets(tx, &dbInfo)
+		tx.Commit()
+	})
 
-	// set up RPC
 	var app = vbeam.NewApplication("HandcraftedForum", db)
 
+	// set up RPC
 	vbeam.RegisterProc(app, AddUser)
 	vbeam.RegisterProc(app, ListUsers)
 
@@ -25,6 +66,7 @@ func MakeApplication() *vbeam.Application {
 
 // REST is a network architecture that enforces a hypermedia constraint, i.e. returns HTML and so is self-describing.
 // This is in contrast to RPC (e.g. JSON), where you would have to know how to interpret fields, i.e. have out-of-band knowledge (server and client are coupled)
+
 
 //var usernames []string
 var usernames = make([]string, 0)
--- frontend/users.tsx
+++ frontend/users.tsx
@@ -3,8 +3,11 @@ import * as vlens from "vlens"
 import * as events from "vlens/events"
 import * as server from "@app/server"
 
+// stable state across rendering cycles
 type Form = {
+    // all names
     data: server.UserListResponse
+    // latest name
     name: string
     error: string
 } 
@@ -30,8 +33,9 @@ export async function fetch(route: string, prefix: string) {
     return server.ListUsers({})
 }
 
-// bind input field to ref
-// stable reference to form without closure
+// this function called on redraws. therefore, cannot have closures if want stability
+// data is immutable from first fetch(), so can be used for cache key
+// TODO: input sanitisation, e.g. no empty, only alphanumeric etc.
 export function view(route: string, prefix: string, data: server.UserListResponse) : preact.ComponentChild {
     let form = useForm(data)
     return <div>
